b04442b656d8aca69bc338a86622dca870826027
Lukas Gianinazzi <orionweissnix@gmail.com>
2012-12-09 12:38:11 +0100

    Initial Commit

---
+++ Cancer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cancer
+//
+//  Created by Lukas on 09.12.12.
+//  Copyright (c) 2012 Lukas Gianinazzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CRAppDelegate class]));
+    }
+}
